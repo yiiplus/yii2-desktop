187c316af89db086aca11e91d2363e43f5e5cd9d
陈红斌 <hongbin.chen@aliyun.com>
2019-01-30 15:27:54 +0800

    注释信息优化

--- migrations/m140602_111327_create_menu_table.php
+++ migrations/m140602_111327_create_menu_table.php
@@ -1,11 +1,20 @@
 <?php
+/**
+ * yiiplus/yii2-desktop
+ *
+ * @category  PHP
+ * @package   Yii2
+ * @copyright 2018-2019 YiiPlus Ltd
+ * @license   https://github.com/yiiplus/yii2-desktop/licence.txt Apache 2.0
+ * @link      http://www.yiiplus.com
+ */
 
 use yii\db\Migration;
 use yii\db\Schema;
 use yiiplus\desktop\components\Configs;
 
 /**
- * Migration table of table_menu
+ * Migration table of menu_table
  */
 class m140602_111327_create_menu_table extends Migration
 {
