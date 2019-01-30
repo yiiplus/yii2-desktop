22a6b1982271833cf6991e32561ecdf6d08d2065
liguangquan <liguangquan@himoca.com>
2019-01-30 15:28:03 +0800

    注释规范

--- widgets/tree/TreeColumn.php
+++ widgets/tree/TreeColumn.php
@@ -1,13 +1,11 @@
 <?php
 /**
- * yiiplus\desktop
- *
- * PHP version 7
+ * yiiplus/yii2-desktop
  *
  * @category  PHP
  * @package   Yii2
- * @author    liguangquan@163.com
- * @copyright 2006-2018 YiiPlus Ltd
+ * @copyright 2018-2019 YiiPlus Ltd
+ * @license   https://github.com/yiiplus/yii2-desktop/licence.txt Apache 2.0
  * @link      http://www.yiiplus.com
  */
 
@@ -24,15 +22,10 @@ use yii\helpers\Html;
 use yii\helpers\Inflector;
 
 /**
- * 生成树形菜单列表
+ * 树形菜单列表
  *
- * PHP version 7
- *
- * @category  PHP
- * @package   Yii2
- * @author    liguangquan@163.com
- * @copyright 2006-2018 YiiPlus Ltd
- * @link      http://www.yiiplus.com
+ * @author liguangquan <liguangquan@163.com>
+ * @since 2.0.0
  */
 class TreeColumn extends Object
 {
