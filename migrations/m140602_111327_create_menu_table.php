c3738d1c16ed60c62e9600976fcc2fa57b3505fe
yangwenhui <yangwenhui@hoolai.com>
2019-02-22 11:34:03 +0800

    menu-bug

--- migrations/m140602_111327_create_menu_table.php
+++ migrations/m140602_111327_create_menu_table.php
@@ -24,22 +24,7 @@ class m140602_111327_create_menu_table extends Migration
      */
     public function up()
     {
-        $menuTable = Configs::instance()->menuTable;
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
-        }
-
-        $this->createTable($menuTable, [
-            'id' => $this->primaryKey(),
-            'name' => $this->string(128)->notNull(),
-            'parent' => $this->integer(),
-            'route' => $this->string(),
-            'order' => $this->integer(),
-            'icon' => $this->string(20),
-            'data' => $this->binary(),
-            "FOREIGN KEY ([[parent]]) REFERENCES {$menuTable}([[id]]) ON DELETE SET NULL ON UPDATE CASCADE",
-        ], $tableOptions);
+        $this->addColumn('admin_menu', 'icon', $this->string(32)->notNUll()->comment('图标'));
     }
 
     /**
@@ -47,6 +32,6 @@ class m140602_111327_create_menu_table extends Migration
      */
     public function down()
     {
-        $this->dropTable(Configs::instance()->menuTable);
+        $this->dropColumn('admin_menu', 'icon');
     }
 }
